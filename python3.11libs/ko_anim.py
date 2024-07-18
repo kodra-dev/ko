f6f7610c8461eac06ed910ae97d92ebe582bc0de
kodra-dev <kodoragon1997@gmail.com>
2024-07-18 18:11:43 +0800

    k

--- python3.11libs/ko_anim.py
+++ python3.11libs/ko_anim.py
@@ -15,13 +15,22 @@ def saveBookmarks(kwargs):
     node_name = node.name()
 
     path = bookmarkFilePath(node_name)
-    hou.anim.saveBookmarks(path)
-    hou.ui.setStatusMessage(f"Saved to {path}")
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+
+    success = hou.anim.saveBookmarks(path)
+    if success:
+        hou.ui.setStatusMessage(f"Saved to {path}")
+    else:
+        raise hou.Error("Failed to save bookmarks")
 
 def loadBookmarks(kwargs):
     node = kwargs['node']
     node_name = node.name()
 
-    hou.anim.loadBookmarks(bookmarkFilePath(node_name))
-    hou.ui.setStatusMessage(f"Loaded from {bookmarkFilePath(node_name)}")
+    success = hou.anim.loadBookmarks(bookmarkFilePath(node_name))
+    if success:
+        hou.ui.setStatusMessage(f"Loaded from {bookmarkFilePath(node_name)}")
+    else:
+        raise hou.Error("Failed to load bookmarks")
     
